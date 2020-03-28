3209c2d54a94cbaafd8b7752b482396f2802d5bc
Zac Hing <zachng1@gmail.com>
2020-03-28 17:20:58 +1300

    setup build

---
+++ precache-manifest.64dc432ef737d64dfc045e3bd802bdcc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7050deab7f7000f5d2867ca6fddeb296",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef19226d5b1ee3d9aecb",
+    "url": "/static/css/main.bc8e9f14.chunk.css"
+  },
+  {
+    "revision": "aab2bb4b7a3dacb14aad",
+    "url": "/static/js/2.188abb24.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.188abb24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef19226d5b1ee3d9aecb",
+    "url": "/static/js/main.65feb199.chunk.js"
+  },
+  {
+    "revision": "1684a468b1ee2902709f",
+    "url": "/static/js/runtime-main.4cc6d467.js"
+  }
+]);
